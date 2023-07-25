e5ecb1c02b56374e11f63a09678623f1e716c5f2
wilfriedy <wilfriedakpe3@gmail.com>
2023-07-25 12:44:14 +0100

    card plc project file setup

---
+++ cardplc/src/data-cards-type.js
@@ -0,0 +1,17 @@
+export default cardsTypeData = [
+  {
+    type: "Create custom cards",
+    details:
+      "Create cards that work exactly as you configured them. Make real-time decisions on charges and spendings.",
+  },
+  {
+    type: "Create custom cards",
+    details:
+      "Create cards that work exactly as you configured them. Make real-time decisions on charges and spendings.",
+  },
+  {
+    type: "Create custom cards",
+    details:
+      "Create cards that work exactly as you configured them. Make real-time decisions on charges and spendings.",
+  },
+];
